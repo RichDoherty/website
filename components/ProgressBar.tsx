df03759fcfe3fbf80185eb839cf1736eb5566943
RichDoherty <doherty622@gmail.com>
2022-11-14 21:18:08 -0500

    Progress Bar

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+
+
+const ProgressBar = () => {
+    const [width, setWidth] = useState(0);
+    const scrollListener = () => {
+        const doc = document.documentElement;
+        let top = doc.scrollTop || document.body.scrollTop;
+        let height = doc.scrollHeight || document.body.scrollHeight;
+        let percent = (top / (height - doc.clientHeight)) * 100;
+
+        setWidth(percent);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollListener);
+        return () => window.removeEventListener("scroll", scrollListener);
+    });
+
+    return (
+        <div style={{ width: width + '%' }} className='fixed h-1 top-16 bg-gradient-to-r from-emerald-500 to-blue-600'></div>
+    )
+}
+
+export default ProgressBar;
