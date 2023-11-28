326ea1b22f778be8696530553f52066858fb150e
RichDoherty <doherty622@gmail.com>
2023-11-28 13:28:16 -0500

    fix code block styles

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,10 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 const Post = defineDocumentType(() => ({
     name: "Post",
@@ -34,7 +40,37 @@ const Post = defineDocumentType(() => ({
     },
 }));
 
+const prettyCodeOptions = {
+    // Use one of Shiki's packaged themes
+    theme: {
+        dark: "vitesse-dark",
+        light: "vitesse-light",
+    },
+    onVisitLine(node) {
+        // Prevent lines from collapsing in `display: grid` mode, and
+        // allow empty lines to be copy/pasted
+        if (node.children.length === 0) {
+            node.children = [{ type: "text", value: " " }];
+        }
+    },
+    // Feel free to add classNames that suit your docs
+    onVisitHighlightedLine(node) {
+        node.properties.className.push("line--highlighted");
+    },
+    onVisitHighlightedWord(node) {
+        node.properties.className = ["word"];
+    },
+};
+
 export default makeSource({
     contentDirPath: "content",
     documentTypes: [Post],
+    mdx: {
+        remarkPlugins: [remarkGfm, remarkFrontmatter],
+        rehypePlugins: [
+            [rehypePrettyCode, prettyCodeOptions],
+            rehypeSlug,
+            rehypeAutolinkHeadings,
+        ],
+    },
 });
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-import nextMDX from "@next/mdx";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
 import { withContentlayer } from "next-contentlayer";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,37 +10,4 @@ const nextConfig = {
     },
 };
 
-const options = {
-    // Use one of Shiki's packaged themes
-    theme: "material-darker",
-    onVisitLine(node) {
-        // Prevent lines from collapsing in `display: grid` mode, and
-        // allow empty lines to be copy/pasted
-        if (node.children.length === 0) {
-            node.children = [{ type: "text", value: " " }];
-        }
-    },
-    // Feel free to add classNames that suit your docs
-    onVisitHighlightedLine(node) {
-        node.properties.className.push("line--highlighted");
-    },
-    onVisitHighlightedWord(node) {
-        node.properties.className = ["word"];
-    },
-};
-
-const withMDX = nextMDX({
-    // By default only the .mdx extension is supported.
-    extension: /\.mdx?$/,
-    options: {
-        providerImportSource: "@mdx-js/react",
-        remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
-        rehypePlugins: [
-            [rehypePrettyCode, options],
-            rehypeSlug,
-            rehypeAutolinkHeadings,
-        ],
-    },
-});
-
-export default withContentlayer(withMDX(nextConfig));
+export default withContentlayer(nextConfig);
